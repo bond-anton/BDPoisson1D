eb40edf292151fa8e34f5225440ced5133808277
bond_anton <bond.anton@gmail.com>
2017-08-01 04:32:58 +0300

    Syncing work on DirichletNonLinear code refactoring

--- BDPoisson1D/__init__.py
+++ BDPoisson1D/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import division, print_function
 from BDPoisson1D.DirichletLinear import dirichlet_poisson_solver, dirichlet_poisson_solver_mesh
 from BDPoisson1D.DirichletLinear import dirichlet_poisson_solver_amr
-from BDPoisson1D.DirichletNonLinear import dirichlet_non_linear_poisson_solver
+from BDPoisson1D.DirichletNonLinear import dirichlet_non_linear_poisson_solver, dirichlet_non_linear_poisson_solver_mesh
+from BDPoisson1D.DirichletNonLinear import dirichlet_non_linear_poisson_solver_amr
 from BDPoisson1D.NeumannLinear import neumann_poisson_solver
