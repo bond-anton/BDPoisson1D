086d7159551a08cba379c33e4b1b772cc34ba8f3
bond-anton <bond.anton@gmail.com>
2020-02-27 15:58:10 +0300

    tests for first order linear ODE solver

--- tests/test_First_Order_Linear.py
+++ tests/test_First_Order_Linear.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from BDMesh import Mesh1DUniform
 from BDPoisson1D.FirstOrderLinear import dirichlet_first_order_solver_arrays, dirichlet_first_order_solver
-# from BDPoisson1D.DirichletLinear import dirichlet_poisson_solver_mesh_arrays, dirichlet_poisson_solver_mesh
+from BDPoisson1D.FirstOrderLinear import dirichlet_first_order_solver_mesh_arrays, dirichlet_first_order_solver_mesh
 # from BDPoisson1D.DirichletLinear import dirichlet_poisson_solver_amr
 from BDPoisson1D.Function import Function, NumericGradient
 
@@ -76,3 +76,32 @@ class TestDirichletFirstOrder(unittest.TestCase):
         result_2 = np.asarray(dirichlet_first_order_solver(nodes, self.p, self.f,
                                                            bc1, bc2, j=1))
         self.assertTrue(np.mean(result_2[:, 1]) < np.mean(result_1[:, 1]))
+
+    def test_dirichlet_first_order_solver_mesh(self):
+        start = -1.0
+        stop = 2.0
+        mesh_1 = Mesh1DUniform(start, stop,
+                               boundary_condition_1=self.y.evaluate([start])[0],
+                               boundary_condition_2=self.y.evaluate([stop])[0],
+                               physical_step=0.02)
+        dirichlet_first_order_solver_mesh_arrays(mesh_1, self.p.evaluate(mesh_1.physical_nodes),
+                                                 self.f.evaluate(mesh_1.physical_nodes))
+        mesh_2 = Mesh1DUniform(start, stop,
+                               boundary_condition_1=self.y.evaluate([start])[0],
+                               boundary_condition_2=self.y.evaluate([stop])[0],
+                               physical_step=0.01)
+        dirichlet_first_order_solver_mesh_arrays(mesh_2, self.p.evaluate(mesh_2.physical_nodes),
+                                                 self.f.evaluate(mesh_2.physical_nodes))
+        self.assertTrue(np.mean(np.asarray(mesh_2.residual)) < np.mean(np.asarray(mesh_1.residual)))
+
+        mesh_1 = Mesh1DUniform(start, stop,
+                               boundary_condition_1=self.y.evaluate([start])[0],
+                               boundary_condition_2=self.y.evaluate([stop])[0],
+                               physical_step=0.02)
+        dirichlet_first_order_solver_mesh(mesh_1, self.p, self.f)
+        mesh_2 = Mesh1DUniform(start, stop,
+                               boundary_condition_1=self.y.evaluate([start])[0],
+                               boundary_condition_2=self.y.evaluate([stop])[0],
+                               physical_step=0.01)
+        dirichlet_first_order_solver_mesh(mesh_2, self.p, self.f)
+        self.assertTrue(np.mean(np.asarray(mesh_2.residual)) < np.mean(np.asarray(mesh_1.residual)))
