be760a308ed8faf5141fee043495f0accd81815d
bond_anton <bond.anton@gmail.com>
2020-02-28 17:01:24 +0300

    First Order linear ODE solver

--- demo/10 - Dirichlet_NL_First_Order.py
+++ demo/10 - Dirichlet_NL_First_Order.py
@@ -56,10 +56,10 @@ p = MixFunction()
 f = TestFunctional(Nd, kT, y)
 df_dy = TestFunctionalDf(Nd, kT, y)
 
-start = -1.0
-stop = 2.0
+start = -1
+stop = 2
 
-nodes = np.linspace(start, stop, num=5001, endpoint=True)  # generate nodes
+nodes = np.linspace(start, stop, num=103, endpoint=True)  # generate nodes
 y0_nodes = y.evaluate(nodes)
 p_nodes = p.evaluate(nodes)
 f_nodes = f.evaluate(nodes)
@@ -68,7 +68,7 @@ df_dy_nodes = df_dy.evaluate(nodes)
 bc1 = 1.0
 bc2 = 3.7
 
-for i in range(1000):
+for i in range(5000):
     result = dirichlet_non_linear_first_order_solver_arrays(nodes, y0_nodes, p_nodes,
                                                             f_nodes, df_dy_nodes,
                                                             bc1, bc2, j=1.0, w=1.0)  # solve Poisson equation
@@ -86,18 +86,18 @@ dy_solution = np.gradient(result[:, 0], nodes, edge_order=2)
 # Plot the result
 fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, sharex=True)
 ax1.plot(nodes, f_nodes, 'r-', label='f(x)')
-ax1.plot(nodes[2:-2], dy_solution[2:-2] + np.asarray(result[2:-2, 0]) * np.asarray(p_nodes[2:-2]), 'b-', label='dy/dx + p(x)*y (solution)')
+ax1.plot(nodes[:], dy_solution[:] + np.asarray(result[:, 0]) * np.asarray(p_nodes[:]), 'b-', label='dy/dx + p(x)*y (solution)')
 ax1.legend()
 
 ax2.plot(nodes, dy_numeric.evaluate(nodes), 'r-', label='dy/dx')
-ax2.plot(nodes[2:-2], dy_solution[2:-2], 'b-', label='dy/dx (solution)')
+ax2.plot(nodes[:], dy_solution[:], 'b-', label='dy/dx (solution)')
 ax2.legend()
 
 ax3.plot(nodes, y.evaluate(nodes), 'r-', label='y(x)')
-ax3.plot(nodes[2:-2], result[2:-2, 0], 'b-', label='solution')
+ax3.plot(nodes[:], result[:, 0], 'b-', label='solution')
 ax3.legend()
 
-ax4.plot(nodes[2:-2], result[2:-2, 1], 'g-o', label='residual')
+ax4.plot(nodes[:], result[:, 1], 'g-o', label='residual')
 ax4.legend()
 plt.show()
 print(np.mean(result[:, 1]))
